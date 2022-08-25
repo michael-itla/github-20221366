4a2648a8b0fe2184d9153e9e5e30708976a67479
michael-itla <107764761+michael-itla@users.noreply.github.com>
2022-08-25 18:33:04 -0400

    Create Sumas de 2 numero enteros.c

---
+++ Sumas de 2 numero enteros.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int numero1;
+    int numero2;
+    int resultado;
+
+    printf("cuale es tu primer nemuero\n");
+    scanf("%d",&numero1);
+       printf("cuale es tu segundo nemuero\n");
+        scanf("%d",&numero2);
+
+    resultado =numero1+numero2;
+
+    printf("la suma es %d\n" ,resultado );
+    resultado++;
+}
